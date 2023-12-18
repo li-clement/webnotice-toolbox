8927c88cf0e75a4a066fa4a6641516bf4a47f956
Clement Li <robekeane@126.com>
2023-12-18 12:37:37 +0000

    fix fetch function
    Signed-off-by: Clement Li <robekeane@126.com>

--- go.mod
+++ go.mod
@@ -3,8 +3,17 @@ module github.com/OpenDataology/webnotice-toolbox
 go 1.16
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/antchfx/htmlquery v1.3.0 // indirect
+	github.com/antchfx/xmlquery v1.3.18 // indirect
 	github.com/gin-gonic/gin v1.9.1
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.10.0
+	google.golang.org/appengine v1.6.8 // indirect
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
 )
--- webnotice-scanner/main.go
+++ webnotice-scanner/main.go
@@ -9,6 +9,56 @@ SPDX-License-Identifier: Apache-2.0
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
 
+	"github.com/gocolly/colly"
+)
+
+type Copyright struct {
+	Id              int    `gorm:"type:int;primary_key;autoIncrement" json:"id"`
+	License_name    string `gorm:"type:TEXT" json:"license_name,omitempty"`
+	Licensor        string `gorm:"type:TEXT" json:"licensor,omitempty"`
+	License_content string `gorm:"type:TEXT" json:"license_content,omitempty"`
+	License_url     string `gorm:"type:TEXT" json:"license_url,omitempty"`
+}
+
+func fetch(url string) (string, error) {
+	c := colly.NewCollector()
+
+	var content string
+	c.OnHTML("p, h1, h2, h3, h4, h5, h6", func(e *colly.HTMLElement) {
+		content += strings.TrimSpace(e.Text) + "\n"
+	})
+
+	err := c.Visit(url)
+	if err != nil {
+		return "", err
+	}
+
+	return content, nil
+}
+
+func main() {
+	var copyright *Copyright
+	content, err := fetch("https://www.baidu.com")
+	if err != nil {
+		return
+	}
+
+	// Possible copyright keywords
+	keywords := []string{"Copyright", "©", "版权", "百度"}
+
+	for _, keyword := range keywords {
+		index := strings.LastIndex(content, keyword)
+		if index != -1 {
+			copyright = &Copyright{
+				License_content: content[index:],
+			}
+			fmt.Println(copyright)
+			fmt.Println(content)
+			return
+		}
+	}
 }
